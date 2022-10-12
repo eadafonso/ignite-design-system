35a78e73b8fe5e33dad79d5d5b95936b1f799dea
Edvaldo Afonso <eadafonso@gmail.com>
2022-10-12 15:07:31 +0100

    chore: :tada: configure acessibility for storybook

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,17 +1,23 @@
-import type { Meta, StoryObj } from '@storybook/react'
-import { Box, BoxProps, Text, Heading } from '@ignite-ui/react'
+import type { Meta, StoryObj } from "@storybook/react";
+import { Box, BoxProps, Text, Heading } from "@ignite-ui/react";
 
 export default {
-  title: 'Surfaces/Box',
+  title: "Surfaces/Box",
   component: Box,
   args: {
     children: (
       <>
         <Text as="strong">Testando o elemento Box</Text>
-        <Heading as="h1">Testando o elemento Box</Heading>
       </>
     ),
   },
-} as Meta<BoxProps>
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
+} as Meta<BoxProps>;
 
-export const Primary: StoryObj<BoxProps> = {}
+export const Primary: StoryObj<BoxProps> = {};
--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,36 +1,60 @@
-import type { Meta, StoryObj } from '@storybook/react'
-import { Button, ButtonProps } from '@ignite-ui/react'
-import { ArrowRight } from 'phosphor-react'
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button, ButtonProps } from "@ignite-ui/react";
+import { ArrowRight } from "phosphor-react";
 
 export default {
-  title: 'Form/Button',
+  title: "Form/Button",
   component: Button,
   args: {
-    children: 'Send',
+    children: "Send",
+    variant: "primary",
+    size: "md",
+    disabled: false,
   },
-} as Meta<ButtonProps>
 
-export const Primary: StoryObj<ButtonProps> = {}
+  argTypes: {
+    variant: {
+      options: ["primary", "secondary", "tertiary"],
+      control: {
+        type: "inline-radio",
+      },
+    },
+    size: {
+      options: ["sm", "md"],
+      control: {
+        type: "inline-radio",
+      },
+    },
+    disabled: {
+      control: {
+        type: "boolean",
+      },
+    },
+    onClick: { action: "clicked" },
+  },
+} as Meta<ButtonProps>;
+
+export const Primary: StoryObj<ButtonProps> = {};
 
 export const Secondary: StoryObj<ButtonProps> = {
   args: {
-    variant: 'secondary',
-    children: 'Create new',
+    variant: "secondary",
+    children: "Create new",
   },
-}
+};
 
 export const Tertiary: StoryObj<ButtonProps> = {
   args: {
-    variant: 'tertiary',
-    children: 'Cancel',
+    variant: "tertiary",
+    children: "Cancel",
   },
-}
+};
 
 export const Small: StoryObj<ButtonProps> = {
   args: {
-    size: 'sm',
+    size: "sm",
   },
-}
+};
 
 export const WithIcon: StoryObj<ButtonProps> = {
   args: {
@@ -41,10 +65,10 @@ export const WithIcon: StoryObj<ButtonProps> = {
       </>
     ),
   },
-}
+};
 
 export const Disabled: StoryObj<ButtonProps> = {
   args: {
     disabled: true,
   },
-}
+};
